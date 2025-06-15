a88a1727945bde620f7ff8e50d401b814082af12
devMarlos <smarlosilan@gmail.com>
2025-06-15 02:02:27 -0400

    CRUD de Cursos

--- api/services/curso_service.py
+++ api/services/curso_service.py
@@ -6,3 +6,23 @@ def cadastrar_curso(curso):
     db.session.add(curso_bd)
     db.session.commit()
     return curso_bd
+
+def listar_cursos():
+    # Retorna todos os registros "query.all()"
+    cursos = curso_model.Curso.query.all()
+    return cursos
+
+def listar_curso_id(id):
+    # Filtrando por id com o query.filter_by e logo depois passando que queremos o primeiro registro da tabela com first()
+    curso = curso_model.Curso.query.filter_by(id=id).first()
+    return curso
+
+def atualiza_curso(curso_anterior, curso_novo):
+    curso_anterior.nome = curso_novo.nome
+    curso_anterior.descricao = curso_novo.descricao
+    curso_anterior.data_publicacao = curso_novo.data_publicacao
+    db.session.commit()
+
+def remove_curso(curso):
+    db.session.delete(curso)
+    db.session.commit()
